5ee80df476c2d157aedaf1c3510766151b0d985b
kydlaugan <kydlaugan71@gmail.com>
2022-12-21 04:54:45 +0100

    initialisation du projet shiny

---
+++ app.R
@@ -0,0 +1,19 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+#chargement de l'interface
+source("ui.R") 
+
+#chargement du server
+source("server.R")
+
+# Run the application 
+shinyApp(ui = ui, server = server)
