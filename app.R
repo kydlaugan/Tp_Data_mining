1d6bb636755e786d8e2f848211ccfd3b94dfe37e
ROBIN170 <77198853+ROBIN170@users.noreply.github.com>
2022-12-29 23:02:52 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -10,6 +10,15 @@
 library(shiny)
 library(shinydashboard)
 dat <- read.table("C:/Users/Robinson/Documents/3115_Projet/SouthGermanCredit.asc", header=TRUE) 
+nom_colonne <- c("status", "duration", "credit_history", "purpose", "amount", 
+                 "savings", "employment_duration", "installment_rate",
+                 "personal_status_sex", "other_debtors",
+                 "present_residence", "property",
+                 "age", "other_installment_plans",
+                 "housing", "number_credits",
+                 "job", "people_liable", "telephone", "foreign_worker",
+                 "credit_risk")
+names(dat) <- nom_colonne
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
   dashboardHeader(title = " INF 3115 PROJET "),
@@ -26,7 +35,7 @@ ui <- dashboardPage(
     tabItems(
       tabItem("Analyse descriptive",h1("Analyse statistique")),
       tabItem("hist",h1("Histogrammes des attributs"),
-              box(selectInput("features","Histogrammes des variables:",c("laufkont","laufzeit","moral","verw","hoehe"),width = 300),width = 4),
+              box(selectInput("features","Histogrammes des variables:",nom_colonne,width = 300),width = 4),
               box(plotOutput("hist") )
       ),
       tabItem("sum",h1(" Vue sommaire des onnées "),
@@ -34,7 +43,7 @@ ui <- dashboardPage(
         verbatimTextOutput("donne")
       ),
       tabItem("dis",h1("Boite a moustache de chaque attribut"),
-              box(selectInput("feature","Boite a moustache:",c("laufkont","laufzeit","moral","verw","hoehe"),width = 300),width = 4),
+              box(selectInput("feature","Boite a moustache:",nom_colonne,width = 300),width = 4),
               box(plotOutput("dis") ),
               strong("Les deux traits horizontaux de la boite
 sont le 1er et le 3e quartiles de l'attribut choisi"),
@@ -47,7 +56,7 @@ sont le 1er et le 3e quartiles de l'attribut choisi"),
 server <- function(input, output) {
 
     output$hist <- renderPlot({
-    plot(dat[[input$features]],xlab=input$features)
+    plot(hist(dat[[input$features]]),xlab=input$features,ylab=input$features)
     })
     output$dis <- renderPlot({
       boxplot(dat[[input$feature]])
