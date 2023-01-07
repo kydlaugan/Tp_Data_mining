65d87ae3915590262329800bae1bed7adddb1b29
ROBIN170 <77198853+ROBIN170@users.noreply.github.com>
2023-01-07 21:21:59 +0100

    Add files via upload

--- App.R
+++ App.R
@@ -50,6 +50,8 @@ colnames(plot_predictions) <- c("status", "duration", "credit_history", "purpose
 library(ggplot2)
 library(gridExtra)
 library(dplyr)
+library(factoextra)
+
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
   dashboardHeader(title = " INF 3115 PROJET "),
@@ -59,7 +61,9 @@ ui <- dashboardPage(
                menuSubItem("Histogrammes des attributs",tabName = "hist"),
                menuSubItem("Summary",tabName = "sum"),
                menuSubItem("Distribution",tabName = "dis"),
-               menuSubItem("knn",tabName = "kppv")
+               menuSubItem("k proches voisins",tabName = "kppv"),
+               menuSubItem("Nuage de Points",tabName = "ndp"),
+               menuSubItem("Clustering",tabName = "cl")
       )
     )
   ),
@@ -67,25 +71,46 @@ ui <- dashboardPage(
     tabItems(
       tabItem("Analyse descriptive",h1("Analyse statistique")),
       tabItem("hist",h1("Histogrammes des attributs"),
+              p("Un Histogramme est un outil utilisé pour résumer des données représentées par intervalles de valeurs "),
               box(selectInput("features","Histogrammes des variables:",nom_colonne,width = 300),width = 4),
-              box(plotOutput("hist") )
+              plotOutput("hist")
       ),
       tabItem("sum",h1(" Vue sommaire des onnées "),
-              p(strong("Chaque Attribut est representé respectivement par son minimum , son premier quartile, la médiane la moyenne ,le troisieme quartile ,et le maximum  "), .noWS = NULL, .renderHook = NULL),
+              p(strong("Chaque attribut est representé respectivement par les paramètres de tendance centrale qui sont la médiane et la moyenne , et ensuite les troisieme et premier quartile , le minimun et le maximum  "), .noWS = NULL, .renderHook = NULL),
               verbatimTextOutput("donne")
       ),
-      tabItem("dis",h1("Boite a moustache de chaque attribut"),
-              box(selectInput("feature","Boite a moustache:",nom_colonne,width = 300),width = 4),
-              box(plotOutput("dis") ),
-              strong("Les deux traits horizontaux de la boite
-sont le 1er et le 3e quartiles de l'attribut choisi"),
-              strong("Le trait fort est la médiane de l'attribut choisi ")
+      tabItem("dis",h1("Boite a moustacheS de chaque attribut"),
+              box(selectInput("feature","Boite a moustache:",nom_colonne,width = 300),width = 6),
+              box(strong("Les deux traits horizontaux de la boite
+sont le 1er et le 3e quartile de l'attribut choisi et le trait fort du milieu représente la médiane qui est de deuxième quartile de l'att
+ribut
+               ")),
+              box(strong("Les extrémités des moustaches sont calculeés en utilisant 1.5 fois l'espace inter-quartile,qui est la distance entre le premier et le troisième quartile
+              ")),
+              plotOutput("dis") 
+              
       ),
       tabItem("kppv",h1("Methode de K plus proches voisins"),
-              box(selectInput("feature"," Entree 1 ",nom_colonne,width = 300),width = 4),
-              box(selectInput("fit"," Entree 2 ",nom_colonne,width = 300),width = 4),
-              box(plotOutput("kppv") )
-      )
+              box(textInput("txt1"," Entree 1 ","",width = 300),width = 6),
+              box(textInput("txt2"," Entree 2 ","",width = 300),width = 6),
+              actionButton(inputId = "id_action", label = "Ok !",
+                           icon = icon("refresh")),
+              
+              plotOutput("kppv" )
+      
+      ),
+      tabItem("ndp",h1("Nuage de Points"),
+              box(selectInput("feature1"," Entree 1 ",nom_colonne,width = 300),width = 6),
+              box(selectInput("fit1"," Entree 2 ",nom_colonne,width = 300),width = 6),
+              plotOutput("ndp" ),
+              
+      ),
+      tabItem(  "cl",
+                box(selectInput("n_cluster","Nombre de clusters",c(2:10),width = 300),width = 6),
+                box(strong("La figure ci-dessous représente le classement non supervisé de 1000 donneés en le nombre de clusters choisi.Chaque cluster représente un groupe.
+                           La classification non supervisée permet de regrouper des données non etiquetées en groupes en se servant des distances qui existent entre eux pour les classer. ")),
+                plotOutput("cl")
+                )
     )  
   ))
 
@@ -93,7 +118,7 @@ sont le 1er et le 3e quartiles de l'attribut choisi"),
 server <- function(input, output) {
   
   output$hist <- renderPlot({
-    plot(hist(dat[[input$features]]),xlab=input$features,ylab=input$features)
+    plot(hist(dat[[input$features]]),xlab=paste(input$features), col="red",main="Histogram ")
   })
   output$dis <- renderPlot({
     boxplot(dat[[input$feature]])
@@ -106,11 +131,26 @@ server <- function(input, output) {
   output$jeu <- renderDataTable(dat)
   
   output$kppv <- renderPlot({
-    plot(ggplot(plot_predictions, aes(duration,status,color=predicted,fill=predicted)) +
+    input$id_action
+    isolate(plot(ggplot(plot_predictions, aes(x=get(input$txt1),y=get(input$txt2),color=predicted,fill=predicted)) +
            geom_point(size=3)+
            ggtitle("Relations des predictions entre les deux variables choisies ")+
            theme(plot.title=element_text(hjust=0.5))+
-           theme(legend.position="none"))
+           theme(legend.position="none"),xlab=paste(input$txt1)))
+
+  })
+  output$ndp <- renderPlot({
+    plot(dat[[input$feature1]],dat[[input$fit1]],xlab="Entree 1",ylab="Entree 2")
+  })
+  output$cl <- renderPlot({
+    data_labels=dat$credit_risk
+    datas=dat[1:20]
+    data_scale=scale(datas)
+    datas=dist(data_scale)
+    km.out = kmeans(data_scale,centers =input$n_cluster)
+    km.clusters = km.out$cluster
+    rownames(data_scale) = paste(dat$credit_risk,1:dim(dat)[1],sep="_")
+    fviz_cluster(list(data=data_scale ,cluster=km.clusters))
   })
   
 }
